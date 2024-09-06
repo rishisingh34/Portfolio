db95352894058f3a05621a7a932975109f0343df
rishisingh34 <singh34rishi@gmail.com>
2024-09-06 16:53:55 +0530

    Refactor About component and update font styles
    This commit refactors the About component by adding paragraphs to provide more information about the developer's skills, education, and passion for turning ideas into reality. Additionally, the commit updates the font styles in the tailwind.config.ts file by removing the unused 'ebGaramond' font.

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -8,13 +8,19 @@ const About = () => {
         <div className="text-4xl font-textMeOne">About...</div>
       </div>
       <div className='font-textMeOne italic text-xl leading-relaxed mb-20 md:text-right  '>
+        <p>
         I thrive at the crossroads of technology and innovation, leveraging my experience with React, Node.js, Express, NestJS, Next.js, and React Native to create impactful solutions.
+        </p>
       </div>
       <div className='font-light md:text-center text-xl tracking-wider leading-relaxed mb-20'>
+        <p>
         I'm a full-stack developer currently pursuing my B.Tech in Computer Science and Engineering. Recently, I've been exploring mobile development with React Native and Expo, aiming to craft engaging, cross-platform applications.
+        </p>
       </div>
       <div className='text-lg font-spaceGrotesk italic font-thin text-left tracking-wide mb-10'>
+        <p>
         I'm passionate about turning ideas into reality and enjoy collaborating with teams to build dynamic and intuitive products. If you need to build an app or website, let's connect and turn your vision into reality!
+        </p>
       </div>
       <div className='text-lg text-link hover:text-blue-700 underline italic cursor-pointer'>
         <a href="https://docs.google.com/document/d/18c-4xbfgMvR0GVAMbMd2ubsg-Bf8kjA2zxtgHyJkRcM/edit?usp=sharing">Download CV</a>
