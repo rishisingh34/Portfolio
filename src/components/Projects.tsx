c953e038aa5c31596e5082fc9bd8badb46030264
rishisingh34 <singh34rishi@gmail.com>
2024-10-01 19:35:55 +0530

    Refactor image styling in Projects component

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -11,7 +11,7 @@ const Projects = () => {
       </div>
 
       <div className="h-[70vh] w-full md:overflow-y-auto">
-        <div className="flex flex-wrap justify-center items-center gap-10 p-5 md:text-5xl text-2xl  underline font-light">
+        <div className="flex flex-wrap justify-center items-center gap-10 p-5 md:text-3xl text-2xl  underline font-light">
           {projects.map((project) => (
             <div key={project.path}>
               <Link href={"/projects"+project.path} className="italic">
@@ -21,6 +21,18 @@ const Projects = () => {
           ))}
           <div>
             <a href="https://github.com/rishisingh34/cyber-secure" target='blank'><div className='italic'>cyber secure</div></a>
+          </div>
+          <div>
+            <a href="https://github.com/rishisingh34/cine_student.git" target='blank'><div className='italic'>CINE'24 backend{"(Student)"}</div></a>
+          </div>
+          <div>
+            <a href="https://github.com/rishisingh34/cine_admin.git" target='blank'><div className='italic'>CINE'24 backend{"(Admin)"}</div></a>
+          </div>
+          <div>
+            <a href="https://cine24exam.netlify.app" target='blank'><div className='italic'>CINE'24 Portal</div></a>
+          </div>
+          <div>
+            <a href="https://github.com/rishisingh34/akgec_edu.git" target='blank'><div className='italic'>akgec-edu{"(backend)"}</div></a>
           </div>
         </div>
       </div>
